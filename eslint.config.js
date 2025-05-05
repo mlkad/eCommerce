07b2d84e7fbb9fa401279de773dd23010a908a40
mikhail <michael.maidan1998@gmail.com>
2025-05-05 11:50:21 +0200

    feat: implement RSS-ECOMM-1_15: Create Prettier script

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import reactPlugin from "eslint-plugin-react";
-import reactConfig from "eslint-plugin-react/configs/recommended.js";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
+import reactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default tseslint.config(
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       react: reactPlugin,
     },
     languageOptions: {
       globals: {
         ...globals.browser,
-        ...globals.node
+        ...globals.node,
       },
       parserOptions: {
         ecmaFeatures: {
-          jsx: true
+          jsx: true,
         },
-        sourceType: "module"
-      }
+        sourceType: 'module',
+      },
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactConfig.rules,
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-no-target-blank": ["error", { enforceDynamicLinks: "always" }]
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': ['error', { enforceDynamicLinks: 'always' }],
     },
     settings: {
       react: {
-        version: "detect"
-      }
-    }
+        version: 'detect',
+      },
+    },
   },
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
 );
